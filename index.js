cedca86ecd0f7d2ee4ccc70478fe68972d465707
bdcii <darrell.chess@gmail.com>
2021-04-19 20:55:27 -0400

    added functionality to view all tables

--- index.js
+++ index.js
@@ -11,28 +11,97 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-    
+
 });
 
 
 function appMenu() {
     inquirer
-    .prompt({
-        name: 'options',
-        type: 'rawlist',
-        message: 'What would you like to do?',
-        choices: [
-            'Add a new department',
-            'Add a new role',
-            'Add a new employee',
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Update an employee role',
-        ],
-    }).then((answer) => {
-        console.log('You selected something!');
-    });
+        .prompt({
+            name: 'options',
+            type: 'rawlist',
+            message: 'What would you like to do?',
+            choices: [
+                'Add a new department',
+                'Add a new role',
+                'Add a new employee',
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Update an employee role',
+            ],
+        }).then((answer) => {
+            switch (answer.options) {
+                case 'Add a new department':
+                    addDept();
+                    break;
+
+                case 'Add a new role':
+                    addRole();
+                    break;
+
+                case 'Add a new employee':
+                    addEmployee();
+                    break;
+
+                case 'View all departments':
+                    viewDept();
+                    break;
+
+                case 'View all roles':
+                    viewRoles();
+                    break;
+
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+
+                case 'Update an employee role':
+                    updateRole();
+                    break;
+
+            };
+        });
 };
 
-appMenu();
+
+
+const addDept = () => {
+
+}
+
+const addRole = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const viewDept = () => {
+    connection.query('SELECT * FROM department', (err, res) => {
+        console.table(res);
+    });
+    appMenu();
+}
+
+const viewRoles = () => {
+    connection.query('SELECT * FROM role', (err, res) => {
+        console.table(res);
+    });
+    appMenu();
+}
+
+const viewEmployees = () => {
+    connection.query('SELECT * FROM employee', (err, res) => {
+        console.table(res);
+    });
+    appMenu();
+}
+
+const updateRole = () => {
+
+}
+
+
+appMenu()
